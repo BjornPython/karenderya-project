79402ab943973d008a67dae76173e60b2cf1408a
Nathan <nathanflores887@gmail.com>
2023-04-12 14:29:49 +0800

    Food items that are not available are now displayed gray.

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 function Food({ food, price, addOrder, decrementOrder, qty = null }) {
     if (food === "rerender") { return } // do not include rerender key
+
     return (
-        <div className='food-container'>
+        <div className='food-container' style={qty <= 0 ? { color: "gray" } : { color: "white" }}>
 
             <div className='food-qty'>
-                <h1>{food}</h1>
+                <h1 >{food}</h1>
                 {qty ? <p>left: {qty}</p> : <></>}
             </div>
             <div className='price-add'>
+
                 <h1>{price} PHP</h1>
-                <div>
+
+                <div style={qty <= 0 ? { display: "none" } : {}}>
                     <p onClick={() => { addOrder(food) }} >add 1</p>
                     <p onClick={() => { decrementOrder(food) }} style={{ color: "red" }} >sub 1</p>
                 </div>
